7a118de0284123cf9bd65e2bd0501785a0f1c763
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-01 21:28:27 +0200

    Further work on checkout functionality (payment views, def checkout and checkout.html)
    working on def checkout function in payment views; 1. authenticated user, if authenticated user has shipping address already associated with his account, then it will be returned in a contect dictionary and during the next checkout it will be autofilled, if not the user needs to fill in the shipping form; 2. guest users need to fill in the shipping adress form; inserting values based on shipping address model and passing the context dictionary into checkout.html, so it can be rendered if the user is authenticated and we already have the shipping address in database

--- ecommerce/payment/views.py
+++ ecommerce/payment/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 
+from .models import ShippingAddress
+
 def checkout(request):
 
-    return render(request, 'payment/checkout.html')
+    if request.user.is_authenticated:                                                                                   #users with accounts, (check if can prefill the form)
+
+        try:                                                                                                            #check if authenticated user has shipping address
+
+            shipping_address = ShippingAddress.objects.get(user=request.user.id)
+
+            context ={'shipping':shipping_address}                                                                      #if the shipping address is found a dictionary is created
+                                                                                                                        # a filled in form can be returned
+            return render(request, 'payment/checkout.html', context=context)
+
+        except:                                                                                                         #authenticated user without shipping address,
+                                                                                                                        #needs to fill in the form
+            return render(request, 'payment/checkout.html')
+
+    else:                                                                                                               #guest user, needs to fill in the form
+
+        return render(request, 'payment/checkout.html')
 
 def payment_success(request):
 
