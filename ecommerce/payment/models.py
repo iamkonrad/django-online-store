3c512d0cd8668e4f08514f46339a60f35899d20f
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-01 15:30:58 +0200

    Creating SHIPPING ADDRESS MODEL in payment. models.py
    if a logged-in users proceeds to checkout his shipping address form will get filled-up automatically, however there is some flexibility ingrained in this model since user model leaves, null and blank fields as True which potentially allows for guest checkouts, I have left commented out options with another user model allowing only authenticated and logged-in users to checkout, the choice depends on specific needs; registering shipping address in payment admin.py' making necessary migrations, which went successfully;

--- ecommerce/payment/models.py
+++ ecommerce/payment/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class ShippingAddress(models.Model):
+
+    full_name=models.CharField(max_length=300)
+
+    email = models.EmailField(max_length=255)
+
+    address1 = models.CharField(max_length=300)
+
+    address2 = models.CharField(max_length=300)                                                                         #2nd address for more flexibility in case there is more
+                                                                                                                        #space needed
+    city = models.CharField(max_length=255)
+
+    postal_code = models.CharField(max_length=255)
+
+
+
+    #optional
+
+    state = models.CharField(max_length=255, null=True, blank=True)
+
+
+    # Foreign key, one_to many relationship                                                                             #a user can have multiple shipping addresses, each shipping
+                                                                                                                        #address belong to one user ONLY
+
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True, blank=True)                                      #cascade= when user deletes his account shipping gets removed
+                                                                                                                        #guest checkouts allowed
+
+
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)                                                           #NO GUEST CHECKOUTS
+
+    class Meta:                                                                                                         #helps to determine the plural of "Shipping Address"
+        verbose_name_plural = 'Shipping Address'
+
+
+    def __str__(self):                                                                                                  #instead of shipping address 1,2 shipping address will be
+                                                                                                                        #created based on the PK Shipping address-1,-2,etc.
+        return 'Shipping Address -' +str(self.id)
