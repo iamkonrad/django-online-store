3ce9669c4510ce1d280a892d1074ea42727304d4
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-01 22:24:43 +0200

    CREATION of ORDER model in payment models.py
    CREATION of ORDER model in payment models.py responsible for representing an order placed by a user on the website; Order model possessing a user field representing one to many relationship, user field also serving as an intermediary field between order and shipping address models;

--- ecommerce/payment/models.py
+++ ecommerce/payment/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+from store.models import Product
+
 
 class ShippingAddress(models.Model):
 
@@ -25,19 +27,40 @@ class ShippingAddress(models.Model):
 
 
     # Foreign key, one_to many relationship                                                                             #a user can have multiple shipping addresses, each shipping
-                                                                                                                        #address belong to one user ONLY
+                                                                                                                        #address belongs to one user ONLY
 
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True, blank=True)                                      #cascade= when user deletes his account shipping gets removed
-                                                                                                                        #guest checkouts allowed
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True, blank=True)                                      #cascade= when user deletes his account shipping details get
+                                                                                                                        #removed;guest checkouts allowed
 
 
-    #user = models.ForeignKey(User, on_delete=models.CASCADE)                                                           #NO GUEST CHECKOUTS
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)                                                           #NO GUEST CHECKOUTS (if needed)
 
     class Meta:                                                                                                         #helps to determine the plural of "Shipping Address"
         verbose_name_plural = 'Shipping Address'
 
 
-    def __str__(self):                                                                                                  #instead of shipping address 1,2 shipping address will be
-                                                                                                                        #created based on the PK Shipping address-1,-2,etc.
+    def __str__(self):                                                                                                  #instead of shipping address 1,2 shipping address will
+                                                                                                                        #be created based on the PK Shipping address-1,-2,etc.
         return 'Shipping Address -' +str(self.id)
+
+
+    class Order(models.Model):
+
+        full_name = models.CharField(max_length=300)
+
+        email = models.EmailField(max_length=255)
+
+        shipping_address = models.TextField(max_length=10000)
+
+        amount_paid = models.DecimalField(max_digits=8,decimal_places=2)
+
+        date_ordered = models.DateTimeField(auto_now_add=True)                                                          #will automatically add the order date
+
+        #Foreign key
+
+        user = models.ForeignKey(User, on_delete=models.CASCADE, null=True,  blank=True)                                #represents the  user who placed the order, one to many, one user
+                                                                                                                        #many orders; user an intermediary field
+        def __str__(self):
+
+            return 'Order - #' + str(self.id)                                                                           #orders will get displayed as Order -# 1 instead of Order (1)
