38ec85e0fd5da391afed4efffc8bba4bc3c452a7
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-01 12:37:10 +0200

    USER NOTIFICATIONS TIMER with JavaScript;
    adding id=message-timer' to headers in base.hml responsible for login logout and account deletion, adding a js file in static/js and creatign a function that will set the message-timer element to be hidden after 2500 miliseconds (2.5s); afterwards adding javascript reference in base.html

---
+++ ecommerce/static/js/app.js
@@ -0,0 +1,15 @@
+var message_timeout = document.getElementById("message-timer");                                  //grabbing message-timer element
+
+
+setTimeout(function()
+{
+
+
+    message_timoeut.style.display="none";
+
+
+},2500)
+
+
+
+
