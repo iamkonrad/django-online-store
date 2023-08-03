c7d6691cf5ab8dfdb16d70b18630c1fe09a5c01c
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-03 23:32:19 +0200

    Basic tests for store app views
    Added pytest.ini file and after installing pytest and pytest:django configuerd basic tests for views locate inside Store views

--- ecommerce/store/tests.py
+++ ecommerce/store/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+import pytest
+from django.test import Client
+from django.urls import reverse
+from .models import Category, Product
 
-# Create your tests here.
+@pytest.fixture
+def category():
+    return Category.objects.create(name='Test Category', slug='test-category')
+
+@pytest.fixture                                                                                                         #test product, dependant on the category "fixture"
+def product(category):
+    return Product.objects.create(title='Test Product', slug='test-product', category=category,price=19.99, image ='images/')
+
+@pytest.mark.django_db
+def test_store():
+    client = Client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert 'all_products' in response.context
+
+@pytest.mark.django_db
+def test_categories():
+    client = Client()
+    response = client.get('/')
+    assert 'all_categories' in response.context
+
+@pytest.mark.django_db
+def test_list_category(category):
+    client = Client()
+    url = reverse('list-category', kwargs={'category_slug': category.slug})
+    response = client.get(url)
+    assert response.status_code == 200
+    assert 'category' in response.context
+
+@pytest.mark.django_db
+def test_product_info(product):
+    client = Client()
+    url = reverse('product-info', kwargs={'product_slug': product.slug})
+    response = client.get(url)
+    assert response.status_code == 200
+    assert 'product' in response.context
+
+
